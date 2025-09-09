d51bfe26daf6f6514d00fb0c96478ca45484dc03
10012853 <10012853@id.ohio.gov>
2025-09-09 11:37:12 -0400

    Update streamlit_app.py
    formatting

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,8 +6,7 @@ import requests
 
 # Write directly to the app
 st.title(f"Customize Your Smoothie :cup_with_straw: ")
-st.write
-(
+st.write(
   """Choose the fruits you want in your custom Smoothie!"""
 )
 
@@ -19,8 +18,7 @@ session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
-ingredients_list = st.multiselect
-(
+ingredients_list = st.multiselect(
     "Choose up to 5 Ingedients"
     , my_dataframe
     , max_selections = 6
